33c96a6da10e41479c5c5bf36d529cc9c17ab049
Brendan Heywood <brendan@catalyst-au.net>
2017-02-21 12:31:50 +1100

    Ad hoc parallel test cleanup

--- tests/test9.sh
+++ tests/test9.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-LOG=/tmp/testcron.log
-rm -f $LOG # remove old log file.
-
 PROCESSES=$1;
 
 # Step 1: Enable cron
-#php admin/cli/cron.php --enable
+php admin/cli/cron.php --enable
 
+# Step 2: Disbale all scheduled tasks so we can focus on ad hoc
 php admin/tool/testtasks/cli/disable_all_tasks.php
 
-# Step 2: Ensure cron will run for at least 10 seconds.
+# Step 3: clear adhoc queue so we only have 1 adhoc task
+php admin/tool/testtasks/cli/clear_adhoc_task_queue.php
+
+# Step 4: Ensure cron will run for at least 10 seconds.
 php admin/tool/testtasks/cli/queue_adhoc_tasks.php --numberoftasks=16 --taskduration=1
 
-# Step 3: Run cron in the background and pipe output to log.
+# Step 5: Run n crons in the background
 for  i in `seq 1 $PROCESSES`; do
     echo "Starting cron $i of $PROCESSES";
     php admin/cli/cron.php | grep 'Ending adhoc task' &
