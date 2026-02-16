484f5a5c21c5da6c204afba157eb1ccc93e5e576
Brendan Heywood <brendan@catalyst-au.net>
2026-02-16 15:21:13 +1100

    Add lock test script

---
+++ MDL-87886-cache-lock-timeout.php
@@ -0,0 +1,68 @@
+<?php
+
+// Detect if web or CLI.
+$isweb = !isset($argv);
+$iscli = !$isweb;
+
+// CLI must define this before including config.php
+if ($iscli) {
+    define('CLI_SCRIPT', true);
+}
+
+require('config.php');
+
+if ($isweb) {
+    header('Content-Type: text/plain; charset=UTF-8');
+}
+
+$values = [];
+$ready = false;
+
+function logg($msg) {
+    echo "$msg\n";
+    error_log($msg);
+}
+
+
+while (!$ready) {
+
+    $ready = true;
+
+    for($c = 1; $c <= 5; $c++) {
+
+        $cache = cache::make('core', 'coursemodinfo');
+        $cachekey = "test$c";
+        $value =  $cache->get_versioned($cachekey, 1);
+
+        if ($value) {
+            logg("Hit $cachekey = '$value'");
+            $values[$c] = $value;
+        } else {
+
+            if ($cache->acquire_lock($cachekey, 0) !== false) {
+                $value =  $cache->get_versioned($cachekey, 1);
+
+                if ($value) {
+                    logg("WAIT and then HIT $cachekey = '$value'");
+                    $values[$c] = $value;
+                } else {
+
+                    $value = "value $c";
+                    logg("LOCK and SET $cachekey = '$value'");
+                    sleep(5);
+                    $cache->set_versioned($cachekey, 1, $value);
+                    $cache->get_versioned($cachekey, 1);
+                    $values[$c] = $value;
+                }
+                $cache->release_lock($cachekey);
+            } else {
+                logg("MISS $cachekey and NO LOCK");
+                $ready = false;
+            }
+        }
+    }
+}
+
+// Now we should have all values
+echo json_encode($values, JSON_PRETTY_PRINT) . "\n";
+
